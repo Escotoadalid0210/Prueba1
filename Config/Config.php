bf341effead41c48e15d0d7ddeacf38a4e98f57d
Francisco Escoto <scotoadalid@gmail.com>
2023-04-17 14:58:54 -0600

    configuraciones

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+    const BASE_URL = "http://localhost/biblioteca1/";
+    const HOST = "localhost";
+    const BD = "biblioteca1_mvc";
+    const DB_USER = "root";
+    const PASS = "1995";
+    const CHARSET = "charset=utf8";
+?>
